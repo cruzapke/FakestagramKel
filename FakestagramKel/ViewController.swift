532ebfe01ec872c0f2ffb724c7eb6aa1008cb2ac
KELLY CRUZ APARICIO D3 <kelly_diplomado@macbook-23.local>
2019-03-30 12:35:41 -0600

    Initial Commit

---
+++ FakestagramKel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FakestagramKel
+//
+//  Created by KELLY CRUZ APARICIO D3 on 3/30/19.
+//  Copyright © 2019 ioslab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
